015d998e4c1b9e2b60755b2ab5cf024d42b03b7a
adictya <aditya.26101@gmail.com>
2023-02-07 14:14:51 +0530

    init

---
+++ components/agoraWrapper.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Agora from "@appbuilder/react";
+
+const AgoraWrapper = () => {
+  return (
+    <div style={{ height: "100vh", width: "100vw", display: "flex" }}>
+      <Agora.View />
+    </div>
+  );
+};
+
+export default AgoraWrapper;

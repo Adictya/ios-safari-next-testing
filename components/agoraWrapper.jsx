97dc3dea0046ecc1219583233e5422113983a1d7
adictya <aditya.26101@gmail.com>
2023-02-08 13:26:11 +0530

    added lmc package

--- components/agoraWrapper.jsx
+++ components/agoraWrapper.jsx
@@ -1,8 +1,14 @@
-'use client';
-
 import Agora from "@appbuilder/react";
+import { useEffect } from "react";
 
 const AgoraWrapper = () => {
+  useEffect(() => {
+    Agora.customize({
+      config: {
+        BACKEND_ENDPOINT: process.env.NEXT_PUBLIC_AGORA_BACKEND,
+      },
+    });
+  }, []);
   return (
     <div style={{ height: "100vh", width: "100vw", display: "flex" }}>
       <Agora.View />

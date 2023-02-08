9ed651ffff98faa7680d94dcad435c22719a43eb
adictya <aditya.26101@gmail.com>
2023-02-08 19:15:46 +0530

    added virtual background package

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 // import AgoraWrapper from '../components/agoraWrapper'
 
 import dynamic from "next/dynamic";
+import Link from "next/link";
 const AgoraWrapper = dynamic(() => import("../components/agoraWrapper"), {
   ssr: false,
 });
 
 export default function Home() {
-  return <AgoraWrapper />;
+  return (
+    <div>
+      <Link href="/test">Go</Link>
+      <AgoraWrapper />
+    </div>
+  );
 }
---
+++ pages/test.js
@@ -0,0 +1,13 @@
+// import AgoraWrapper from '../components/agoraWrapper'
+
+import dynamic from "next/dynamic";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      Hi
+      <Link href="/">GoBack</Link>
+    </div>
+  );
+}
